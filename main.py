fa2438c4cca7c96c3a168ae805108459e42165a8
adumitre <andrewdumitrescu@gmail.com>
2025-07-01 22:21:16 -0500

    refined report, added sorting of characters

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
-from stats import get_word_count, char_count
+from stats import (
+  get_word_count,
+  char_count,
+  sort_dict,
+)
 
 def get_book_text(file_path):
   with open(file_path) as f:
     book_text = f.read()
     return book_text
+  
+def print_report(words, chars, book_path):
+  print("============ BOOKBOT ============")
+  print(f"Analyzing book found at {book_path}...")
+  print("----------- Word Count ----------")
+  print(f"Found {words} total words")
+  print("--------- Character Count -------")
+  for item in chars:
+    for key in item:
+      print(f"{key}: {item[key]}")
+  print("============= END ===============")
 
 def main():
   book_path = "./books/frankenstein.txt"
   text = get_book_text(book_path)
   words = get_word_count(text)
-  chars = char_count(text)
-  print(f"{words} words found in the document")
-  print(chars)
+  chars = sort_dict(char_count(text))
+  print_report(words, chars, book_path)
   return
 
 main()
--- stats.py
+++ stats.py
@@ -6,8 +6,22 @@ def char_count(text):
   lowercase = text.lower()
   counts = {}
   for char in lowercase:
-    if char not in counts:
-      counts[char] = 1
-    else:
+    if char in counts:
       counts[char] += 1
+    else:
+      counts[char] = 1
   return counts
+
+def sort_dict(dict_char):
+  sorted_dict = [{k: v} for k, v in dict_char.items()]
+  sorted_dict = alphanum_only(sorted_dict)
+  sorted_dict.sort(key=lambda item: list(item.values())[0], reverse=True)
+  return sorted_dict
+
+def alphanum_only(dict_list):
+  alphanum = []
+  for item in dict_list:
+    for key in item:
+      if key.isalpha():
+        alphanum.append(item)
+  return alphanum
